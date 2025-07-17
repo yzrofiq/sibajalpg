2840ecd202bea7a332ecca3abebbf9fbe2ed9f7b
yzrofiq <yzrofiq@gmail.com>
2025-07-17 10:19:28 +0700

    final

--- resources/views/users/home.blade.php
+++ resources/views/users/home.blade.php
@@ -241,7 +241,7 @@
         </div>
       </div>
 
-      <div id="carouselPDF" class="carousel slide" data-bs-ride="carousel" data-bs-interval="3000">
+      <div id="carouselPDF" class="carousel slide" data-bs-ride="carousel" data-bs-interval="4000">
         <div class="carousel-inner">
           <!-- SLIDE 1 -->
           <div class="carousel-item active">
@@ -632,7 +632,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 @endpush
-
 
 
 
